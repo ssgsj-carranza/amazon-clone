1ab057582b76ca5a2787b161ac1bfed731f26346
unknown <ssgsj.carranza@gmail.com>
2021-07-06 11:56:00 -0500

    initial set up for project, using next js, added config page for domains

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Header from "../components/Header/Header";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+      <h1>Hello</h1>
+      <Header/>
+    </div>
+  );
+}
