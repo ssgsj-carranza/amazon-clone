2a86529465ca338f5486f0a51f7befab70cd3624
unknown <ssgsj.carranza@gmail.com>
2021-07-06 12:42:06 -0500

    added greeting component to navbar, and return and orders as well as a shoppingcart, that will display items in cart, thought its hard coded right now

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,8 +16,24 @@ function Header() {
                 </div>
                 {/* search */}
                 <div className='hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500'>
-                    <input className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none' type="text" />
+                    <input className='p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4' type="text" />
                     <SearchIcon className='h-12 p-4'/>
+                </div>
+                {/* right components of navbar */}
+                <div className='text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap'>
+                    <div className='link'>
+                        <p>Hello Jorge Carranza</p>
+                        <p className='font-extrabold md:text-sm'>Account & Lists</p>
+                    </div>
+                    <div className='link'>
+                        <p>Returns</p>
+                        <p className='font-extrabold md:text-sm'>& Orders</p>
+                    </div>
+                    <div className='relative link flex items-center'>
+                        <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
+                        <ShoppingCartIcon className='h-10' />
+                        <p className='hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
+                    </div>
                 </div>
             </div>
             {/* bottom navbar */}
