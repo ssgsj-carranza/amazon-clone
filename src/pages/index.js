9033f94b0d265764c1501bf642bd5b8ab833e683
unknown <ssgsj.carranza@gmail.com>
2021-07-07 14:30:53 -0500

    fixed bug where banner wasnt displaying, by importing react responsive carousel css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header/Header";
+import Banner from "../components/Banner/Banner";
 
 export default function Home() {
   return (
@@ -8,6 +9,11 @@ export default function Home() {
         <title>Amazon 2.0</title>
       </Head>
       <Header/>
+      <main className='max-w-screen-2xl mx-auto'>
+        {/* Banner */}
+        <Banner />
+        {/* Product Feed */}
+      </main>
     </div>
   );
 }
