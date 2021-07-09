a4fea80ffc893915a1b29fe50c94d983a6347864
unknown <ssgsj.carranza@gmail.com>
2021-07-08 21:45:06 -0500

    refactored mapping function on product rendering side, created a reusable component in product js to store object data, started displaying product images on feed as well

--- src/components/ProductFeed/ProductFeed.js
+++ src/components/ProductFeed/ProductFeed.js
@@ -1,11 +1,19 @@
-// import React from 'react'
+import Product from "../Product/Product"
 
+//--> WHEN MAPPING DONT FORGET TO ADD A KEY!!
 function ProductFeed({products}) {
     return (
         <div>
            <h1>Products</h1>
-           {products.map((product) => (
-               <p>{product.title}</p>
+           {products.map(({id, title, price, description, category, image}) => (
+               <Product
+                    key={id}
+                    id={id}
+                    title={title}
+                    description={description}
+                    category={category}
+                    image={image}
+                    price={price}/>
            ))} 
         </div>
     )
