53f0c7b8474a05adbceb34abcafc3ed08e3568db
unknown <ssgsj.carranza@gmail.com>
2021-07-08 21:53:38 -0500

    created an array to map through the rating star icon, eventually given random ratings to each product instead of a hard coded one

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Image from 'next/dist/client/image';
+import {StarIcon} from '@heroicons/react/solid';
 
 function Product({id, title, price, description, category, image}) {
+    const[rating] = useState(2)
+
     return (
         <div>
             <p>{category}</p>
-            <Image src={image} height={200} width={200} objectFit='contain'/>        
+            <Image src={image} height={200} width={200} objectFit='contain'/>
+            <h4>{title}</h4>
+            <div className='flex'>
+                {Array(rating).fill().map((_, i) => (
+                    <StarIcon className='h-5'/>
+                ))}
+            </div>
         </div>
     )
 }
