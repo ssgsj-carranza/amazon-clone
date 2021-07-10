854465d2c384a6fc5cbd5db7d0c95ec77e32d4da
unknown <ssgsj.carranza@gmail.com>
2021-07-09 22:08:25 -0500

    began workl on styling checkout page

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,10 +1,25 @@
+import Image from 'next/image'
 import React from 'react'
 import Header from '../components/Header/Header'
 
 function Checkout() {
     return (
         <div className='bg-gray-100'>
-            <Header/>      
+            <Header/>
+
+            <main className='lg:flex max-w-screen-2xl mx-auto'>
+                {/* left section */}
+                <div>
+                    <Image src='https://links.papareact.com/ikj' 
+                            width={1020} 
+                            height={250}
+                            objectFit='contain'/>
+                </div>
+
+                {/* right section */}
+                <div></div>
+
+            </main>      
         </div>
     )
 }
