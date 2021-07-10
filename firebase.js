de6219be7dfad08362f4501d4221786767f00190
unknown <ssgsj.carranza@gmail.com>
2021-07-09 21:30:19 -0500

    set up firebase on the back end and enabled google authentication, set up google id key and secret key on virtual environment file

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA4ZOog0pfFRtRU0sFwoAQ8IKBw5LBvfp8",
+    authDomain: "clone-5f3a6.firebaseapp.com",
+    projectId: "clone-5f3a6",
+    storageBucket: "clone-5f3a6.appspot.com",
+    messagingSenderId: "66760696848",
+    appId: "1:66760696848:web:27a19d28dd44aecb595695"
+  };
