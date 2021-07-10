977b86148135c410d63343e14352a6db8944ed56
unknown <ssgsj.carranza@gmail.com>
2021-07-09 22:24:06 -0500

    using redux to work on adding item to basket, set up dispatch const and add to basket slice

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,16 +2,24 @@ import React, {useState} from 'react'
 import Image from 'next/dist/client/image';
 import {StarIcon} from '@heroicons/react/solid';
 import Currency from 'react-currency-formatter';
+import { useDispatch } from 'react-redux';
+import { addToBasket } from '../../slices/basketSlice';
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 function Product({id, title, price, description, category, image}) {
+    const dispatch = useDispatch();
+
     const[rating] = useState(
         Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING 
     );
 
     const [hasPrime] = useState(Math.random() < 0.5)
+    
+    const addItemToBasket = () => {
+        
+    }
 
     return (
         <div className='relative flex flex-col m-5 bg-white z-30 p-10'>
@@ -33,7 +41,7 @@ function Product({id, title, price, description, category, image}) {
                     <p className='text-xs text-gray-500'>FREE Next-Day Delivery</p>
                 </div>
             )}
-            <button className='mt-auto button'>Add to Basket</button>
+            <button onClick={addItemToBasket} className='mt-auto button'>Add to Basket</button>
         </div>
     )
 }
