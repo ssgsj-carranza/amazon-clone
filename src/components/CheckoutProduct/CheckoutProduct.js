a4878f786f3ad44a7f0e289a8d6c0ce681cb8a48
unknown <ssgsj.carranza@gmail.com>
2021-07-12 22:46:24 -0500

    pulled ratings and product title to be displayed on basket page

--- src/components/CheckoutProduct/CheckoutProduct.js
+++ src/components/CheckoutProduct/CheckoutProduct.js
@@ -1,3 +1,4 @@
+import { StarIcon } from '@heroicons/react/outline'
 import Image from 'next/image'
 import React from 'react'
 
@@ -5,6 +6,14 @@ function CheckoutProduct({id, title, price, rating, description, category, image
     return (
         <div className='grid grid-cols-5'>
             <Image src={image} height={200} width={200} objectFit='contain'/>
+            <div className='col-span-3 mx-5'>
+                <p>{title}</p>
+                <div>
+                    {Array(rating).fill().map((_, i) => (
+                        <StarIcon className='h-5 text-yellow-500'/>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
