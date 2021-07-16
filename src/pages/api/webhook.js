2201f4f3ef78b19562ca8b8f05431207e79686e0
unknown <ssgsj.carranza@gmail.com>
2021-07-16 10:14:25 -0500

    created a connection to firebase from back end, working on establishing a connection to stripe next

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -1,7 +1,17 @@
+import { buffer } from "micro";
+import * as admin from 'firebase-admin';
 
+//SECURE A CONNECTION TO FIREBASE FROM THE BACKEND
+const serviceAccount = require('../../../permissions.json');
+const app = !admin.apps.length ? admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+}) : admin.app();
+
+//ESTABLISH CONNECTION TO STRIPE
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async (req, res) => {
     if (req.method === 'POST') {
-
+        
     }
 }
