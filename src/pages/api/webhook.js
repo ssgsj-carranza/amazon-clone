ea3b78cdd1e6f82a29e63be118c8b02f0ffa5b99
unknown <ssgsj.carranza@gmail.com>
2021-07-16 10:26:01 -0500

    worked on verifying the event posted came from stripe, using a catch method, as well as a way to handle the checkout session completed event, working on fulfilling order

--- src/pages/api/webhook.js
+++ src/pages/api/webhook.js
@@ -9,9 +9,33 @@ const app = !admin.apps.length ? admin.initializeApp({
 
 //ESTABLISH CONNECTION TO STRIPE
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+
+const fulfillOrder = async(session) => {
+    // console.log('fulfilling order', session)
+}
 
 export default async (req, res) => {
     if (req.method === 'POST') {
+       const requestBuffer = await buffer(req);
+       const payload = requestBuffer.toString();
+       const sig = req.headers['stripe-signature'];
+       
+       let event;
         
+       //VERIFY THAT THE EVENT POSTED CAME FROM STRIPE
+       try {
+            event = stripe.webhooks.constructEvent(payload, sig, endpointSecret);
+       } catch (err) {
+           console.log('Error', err.message)
+           return res.status(400).send(`webhook error: ${err.message}`)
+       }
+
+       //HANDLE THE CHECKOUT SESSION COMPLETED EVENT
+       if (event.type === 'checkout.session.completed') {
+           const session = event.data.object;
+
+           //FULFILL THE ORDER
+       }
     }
-}
+};
