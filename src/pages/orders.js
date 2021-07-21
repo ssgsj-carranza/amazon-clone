909389c4c46eff0353eca452067a207863de4c4b
unknown <ssgsj.carranza@gmail.com>
2021-07-20 19:55:06 -0500

    created orders function to render out fulfilled orders, using props, began some styling as well on your orders page

---
+++ src/pages/orders.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import {useSession} from 'next-auth/client';
+
+function Orders({orders}) {
+    const [session] = useSession();
+    
+    return (
+        <div>
+            <Header />
+            <main className='max-w-screen-lg mx-auto p-10'>
+                <h1 className='text-3xl border-b mb-2 pb-1 border-yellow-400'>Your Orders</h1>
+                {session ? (
+                    <h2>x Orders</h2>
+                ): (
+                    <h2>Please sign in to see your orders</h2>
+                )}
+            </main>
+        </div>
+    )
+}
+
+export default Orders
